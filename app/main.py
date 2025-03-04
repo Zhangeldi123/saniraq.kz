6545da433f8fda07005e9480b13e397721160e11
Zhangeldi <zhanikshakimurat@gmail.com>
2025-03-05 00:03:51 +0500

    change url

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi import APIRouter, FastAPI, Response, Request
-from api.user import user_router
-from api.house import house_router
+from .api.user import user_router
+from .api.house import house_router
 
 app = FastAPI()
 app.include_router(user_router)
