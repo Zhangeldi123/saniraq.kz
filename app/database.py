b67cc7e2626c26f2d9a3d6e9d00554f6edae6ef4
Zhangeldi <zhanikshakimurat@gmail.com>
2025-03-04 22:50:13 +0500

    changes

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:123@localhost:8000/houses"
+DATABASE_URL = "postgresql://datahouse_vhx1_user:ZzaDPo9TEJo5zQxWuoBNFtWd1tTjcjPo@dpg-cv3jpa0gph6c738segl0-a/datahouse_vhx1"
 
 engine = create_engine(DATABASE_URL)
 
